4d38af02dc9cfc10f28727ab08ed83fff47133f9
Thomas HUET <huet@lemonde.fr>
2014-06-18 11:53:47 +0200

    Allow cross site requests

--- src/server.ml
+++ src/server.ml
@@ -4,7 +4,9 @@ module S = Cohttp_lwt_unix.Server
 let make_server f port =
   let callback conn_id req body =
     lwt body = Cohttp_lwt_body.to_string body in
-    S.respond_string ~status:`OK ~body:(f body) ()
+    S.respond_string
+      ~headers:(Cohttp.Header.init_with "Access-Control-Allow-Origin" "*")
+      ~status:`OK ~body:(f body) ()
   in
   let conn_closed conn_id () = () in
   S.create ~address:"0.0.0.0" ~port {S.callback; conn_closed}
